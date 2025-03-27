4acf99adb203781138adf9a7f9f15544136a376c
K. Papadopoulos <kostashope@hotmail.com>
2025-03-27 18:32:10 +0200

    post

--- myapp2/app.js
+++ myapp2/app.js
@@ -2,15 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send("This is the home page");
 });
 
 app.post('/user', (req, res) => {
   let data = req.body;
+  let name = req.body.username;
+  let email = req.body.email;
 
   console.log(data);
-  res.send("User page");
+  res.send(`User data ${username}, ${email}`);
+})
+
+app.post('userForm', (req, res) => {
+  let data = req.body
+
+  console.log("Data", data);
+  res.send("UserForm Page")
 })
 
 app.listen(port, () => {
